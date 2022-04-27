dbaf80ed8ee1d9a157e353af47da37a5693b4b2a
joricky91 <jonathanricky91@gmail.com>
2022-04-27 11:37:23 +0700

    implement mvvm

---
+++ Nano Challenge 1/View/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Nano Challenge 1
+//
+//  Created by Jonathan Ricky Sandjaja on 25/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = AnimeViewModel()
+    
+//    init() {
+//        UITableView.appearance().backgroundColor = .red
+//    }
+    
+    var body: some View {
+        
+        NavigationView {
+            List{
+                
+                ForEach(arrayofDay, id: \.self) { day in
+                    
+                    Section(header:
+                                
+                        HStack {
+        
+                            Text(day)
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                                
+                            Spacer()
+                            
+                            Button(action: {
+                                viewModel.alertView()
+                            }, label: {Image(systemName: "plus.circle")})
+                        
+                        }){
+                            ForEach(viewModel.animeArr) { daily in
+                                if daily.day.lowercased() == day.lowercased() {
+                                    Text(daily.title)
+                                }
+                            
+                            }
+                            .onDelete(perform: viewModel.delete)
+                        }
+                
+                }
+                
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Anime Watchlist")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Nano Challenge 1/ViewModel/AnimeViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  Nano Challenge 1
+//
+//  Created by Jonathan Ricky Sandjaja on 27/04/22.
+//
+
+import Foundation
+import SwiftUI
+
+class AnimeViewModel: ObservableObject {
+    
+    var day: String = ""
+    @Published var animeArr: [Anime] = arrayOfAnime
+    @Published var anime: String = ""
+    @Published var animeDay: String = ""
+    
+    func alertView() {
+        let alert = UIAlertController(title: "Add New Anime", message: "Add the anime that was released on \(day)", preferredStyle: .alert)
+        
+        alert.addTextField { anime in
+            anime.placeholder = "Insert anime title..."
+        }
+        
+        alert.addTextField { day in
+            day.placeholder = "Insert day..."
+        }
+        
+        let addAnime = UIAlertAction(title: "Add", style: .default, handler: {(_) in
+            self.anime = alert.textFields![0].text!
+            self.animeDay = alert.textFields![1].text!
+//            let newAnime = Anime(title: self.anime, day: self.day)
+            self.animeArr.append(Anime(title: self.anime, day: self.animeDay))
+        })
+        let cancelAnime = UIAlertAction(title: "Cancel", style: .destructive, handler: {(_) in})
+       
+        alert.addAction(addAnime)
+        alert.addAction(cancelAnime)
+        
+        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: {})
+    }
+    
+    func delete(at offsets: IndexSet) {
+        self.animeArr.remove(atOffsets: offsets)
+    }
+
+}
